e84868937ad4d8f4ea2cf60c394e655311f47487
BernardoCosta <bernardopintocosta@gmail.com>
2016-04-13 12:54:22 +0100

    comment test

--- Prototipo/src/backend/bd.java
+++ Prototipo/src/backend/bd.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//comment
 package backend;
 import java.sql.*;
 import java.text.SimpleDateFormat;
